9911877b946faad769f21adb8f8d8effa3ac0dfb
jkjk_kang <etual118@naver.com>
2018-04-12 23:19:00 +0900

    fisrt complete

--- xv6-public/test_yield.c
+++ xv6-public/test_yield.c
@@ -16,8 +16,6 @@ main(int argc, char *argv[])
 			printf(1,"Paren\n");
 			yield();
 	}
-<<<<<<< HEAD
-=======
 
 	if(pid == 0) { //0이면 자식 프로세스
 		printf(1, "parent\n");
@@ -25,7 +23,6 @@ main(int argc, char *argv[])
 	} else { //부모프로세스
 		printf(1, "child\n");
 		exit();
->>>>>>> 291335925149fc98ac10c7492d9a604d1d0e9568
 	}
 	return 0;
 }
