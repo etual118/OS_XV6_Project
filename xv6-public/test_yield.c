52fbee130740b5e857b8e0435db80049b0db7aa5
JKKang <etual118@hanyang.ac.kr>
2018-04-12 23:23:49 +0900

    y_test

--- xv6-public/test_yield.c
+++ xv6-public/test_yield.c
@@ -16,20 +16,6 @@ main(int argc, char *argv[])
 			printf(1,"Paren\n");
 			yield();
 	}
-
-	if(pid == 0) { //0이면 자식 프로세스
-		printf(1, "parent\n");
-		exit();
-	} else { //부모프로세스
-		printf(1, "child\n");
-		exit();
-<<<<<<< HEAD
-	
-        }
-        }
-=======
 	}
->>>>>>> 9911877b946faad769f21adb8f8d8effa3ac0dfb
 	return 0;
 }
-
