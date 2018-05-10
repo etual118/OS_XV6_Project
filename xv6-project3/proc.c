c60cc558ebd24c3fe0e6220a9822ac5d1b87a481
jkjk_kang <etual118@naver.com>
2018-05-11 03:32:08 +0900

    project3 proto

--- xv6-project3/proc.c
+++ xv6-project3/proc.c
@@ -48,7 +48,7 @@ thread_exit(void *retval){
   curproc->tinfo.master->ret[curproc->tinfo.tid] = retval;
   curproc->state = ZOMBIE; //How to atomic??
   curproc->tinfo.master->cnt_t--;
-  cprintf("here\n");
+  cprintf("here i am %d and master %d\n", curproc->tinfo.tid, curproc->tinfo.master->cnt_t);
   sched();
   panic("zombie exit");
 }
