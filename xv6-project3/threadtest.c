74827d1bcacb1eda219fecc668bce634ff4a7222
jkjk_kang <etual118@naver.com>
2018-05-11 19:52:53 +0900

    project3 proto

--- xv6-project3/threadtest.c
+++ xv6-project3/threadtest.c
@@ -617,7 +617,9 @@ stridetest1(void)
     }
   }
   flag = 1;
+  printf(1, "create fin %d\n", pid);
   sleep(500);
+  printf(1, "sleep fin %d\n", pid);
   flag = 0;
   for (i = 0; i < NUM_THREAD; i++){
     if (thread_join(threads[i], &retval) != 0){
