e99d5a5ad25ba652c986007ce4c811e10339647e
jkjk_kang <etual118@naver.com>
2018-05-20 19:50:44 +0900

    pj3

--- xv6-project3/proc.c
+++ xv6-project3/proc.c
@@ -49,6 +49,7 @@ thread_exit(void *retval){
   acquire(&ptable.lock);
   // Master might be sleeping in thread_join().
   wakeup1(curproc->tinfo.master);
+  struct proc *p;
   // Save retval in master thread's proc structure.
   curproc->tinfo.master->ret[curproc->tinfo.tid] = retval;
   curproc->state = ZOMBIE; 
@@ -243,12 +244,9 @@ growproc(int n)
 int
 fork(void)
 {
-  int i, pid, is_master = 0;
+  int i, pid;
   struct proc *np;
   struct proc *curproc = myproc();
-  struct proc *master = call_master();
-  if(curproc == master)
-    is_master = 1;
 
   // Allocate process.
   if((np = allocproc()) == 0){
@@ -277,19 +275,7 @@ fork(void)
   safestrcpy(np->name, curproc->name, sizeof(curproc->name));
 
   pid = np->pid;
-  if(!is_master){
-    for(i = 0; i < NTHREAD; i++){
-      if(master->threads[i] != 0 && master->threads[i] != curproc){
-        thread_clear(master->threads[i]);
-      }
-    } 
-    curproc->prior = master->prior;
-    curproc->pticks = master->pticks;
-    curproc->myst = master->myst;
-    change_master(curproc, master);
-    curproc->tinfo.master = 0;
-    thread_clear(master);
-  }
+
   acquire(&ptable.lock);
 
   np->state = RUNNABLE;
