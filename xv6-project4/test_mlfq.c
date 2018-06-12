7c635a1791dc619c8edc4a57fb0944ccd13a3b3f
jkjk_kang <etual118@naver.com>
2018-06-12 23:48:01 +0900

    wiki added

--- xv6-project4/test_mlfq.c
+++ xv6-project4/test_mlfq.c
@@ -16,6 +16,7 @@ main(int argc, char *argv[])
     int r;
     int total;
     char *path = (argc > 1) ? argv[1] : "hugefile";
+    char *path2 = (argc > 1) ? argv[1] : "hugefile2";
     char data[BUFSIZE];
     char buf[BUFSIZE];
 
@@ -39,49 +40,62 @@ main(int argc, char *argv[])
     printf(1, "%d bytes written\n", BUF_PER_FILE * BUFSIZE);
     close(fd);
 
-    printf(1, "2. read test\n");
-    fd = open(path, O_RDONLY);
-    for (i = 0; i < BUF_PER_FILE; i++){
+    printf(1, "1. create test\n");
+    fd = open(path2, O_CREATE | O_RDWR);
+    for(i = 0; i < BUF_PER_FILE; i++){
         if (i % 100 == 0){
-            printf(1, "%d bytes read\n", i * BUFSIZE);
+            printf(1, "%d bytes written\n", i * BUFSIZE);
         }
-        if ((r = read(fd, buf, sizeof(data))) != sizeof(data)){
-            printf(1, "read returned %d : failed\n", r);
+        if ((r = write(fd, data, sizeof(data))) != sizeof(data)){
+            printf(1, "write returned %d : failed\n", r);
             exit();
         }
-        for (j = 0; j < sz; j++) {
-            if (buf[j] != data[j]) {
-                printf(1, "data inconsistency detected\n");
-                exit();
-            }
-        }
     }
-    printf(1, "%d bytes read\n", BUF_PER_FILE * BUFSIZE);
+    printf(1, "%d bytes written\n", BUF_PER_FILE * BUFSIZE);
     close(fd);
+    // printf(1, "2. read test\n");
+    // fd = open(path, O_RDONLY);
+    // for (i = 0; i < BUF_PER_FILE; i++){
+    //     if (i % 100 == 0){
+    //         printf(1, "%d bytes read\n", i * BUFSIZE);
+    //     }
+    //     if ((r = read(fd, buf, sizeof(data))) != sizeof(data)){
+    //         printf(1, "read returned %d : failed\n", r);
+    //         exit();
+    //     }
+    //     for (j = 0; j < sz; j++) {
+    //         if (buf[j] != data[j]) {
+    //             printf(1, "data inconsistency detected\n");
+    //             exit();
+    //         }
+    //     }
+    // }
+    // printf(1, "%d bytes read\n", BUF_PER_FILE * BUFSIZE);
+    // close(fd);
 
-    printf(1, "3. stress test\n");
-    total = 0;
-    for (i = 0; i < NUM_STRESS; i++) {
-        printf(1, "stress test...%d \n", i);
-        if(unlink(path) < 0){
-            printf(1, "rm: %s failed to delete\n", path);
-            exit();
-        }
+    // printf(1, "3. stress test\n");
+    // total = 0;
+    // for (i = 0; i < NUM_STRESS; i++) {
+    //     printf(1, "stress test...%d \n", i);
+    //     if(unlink(path) < 0){
+    //         printf(1, "rm: %s failed to delete\n", path);
+    //         exit();
+    //     }
 
-        fd = open(path, O_CREATE | O_RDWR);
-        for(j = 0; j < BUF_PER_FILE; j++){
-            if (j % 100 == 0){
-                printf(1, "%d bytes totally written\n", total);
-            }
-            if ((r = write(fd, data, sizeof(data))) != sizeof(data)){
-                printf(1, "write returned %d : failed\n", r);
-                exit();
-            }
-            total += sizeof(data);
-        }
-        printf(1, "%d bytes written\n", total);
-        close(fd);
-    }
+    //     fd = open(path, O_CREATE | O_RDWR);
+    //     for(j = 0; j < BUF_PER_FILE; j++){
+    //         if (j % 100 == 0){
+    //             printf(1, "%d bytes totally written\n", total);
+    //         }
+    //         if ((r = write(fd, data, sizeof(data))) != sizeof(data)){
+    //             printf(1, "write returned %d : failed\n", r);
+    //             exit();
+    //         }
+    //         total += sizeof(data);
+    //     }
+    //     printf(1, "%d bytes written\n", total);
+    //     close(fd);
+    // }
 
     exit();
 }
